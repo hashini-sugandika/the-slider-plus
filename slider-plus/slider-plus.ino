1afdbe7631b2d5bc6daffcf422009f886217746e
VinuraGallage <113351336+th3-s7r4ng3r@users.noreply.github.com>
2023-05-15 16:20:55 +0530

    Basic Structure added to the program

---
+++ slider-plus/slider-plus.ino
@@ -0,0 +1,50 @@
+//########## Basic Setup & Code ##########
+void setup() {
+
+
+}
+
+void loop() {
+
+
+}
+
+
+//########## Front-end Modules ##########
+//------Authentication Modules-----
+//RFID Sensor reading
+void RFIDRead(){
+}
+//NumberPad reading
+void NumPadRead(){
+}
+//Fingerprint reading
+void FingerprintRead(){
+}
+
+//--------Other Modules-------
+//Closing the door when someone leaves the room using motion sensor
+int ReadMotionSens(){
+}
+//Buzzer write when opening and closing the door
+void WriteToBuzzer(){
+}
+
+
+//########## Back-end Modules ##########
+//Opening Door from the inside using touch sensor
+void ReadTouchSens(){
+}
+//Reading configurations from the SD Card
+void ReadSDCard(){
+}
+//Reading the Door sensor to verify door status
+void ReadDoorSens(){
+}
+//Controlling the door using the motor
+void ControlDoor(){
+}
+
+
+
+

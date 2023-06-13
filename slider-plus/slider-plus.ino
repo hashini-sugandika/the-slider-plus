d4d4e1967e890e0dc4eb4bf22bd7d07a2b30d5d1
VinuraGallage <113351336+th3-s7r4ng3r@users.noreply.github.com>
2023-06-13 13:02:43 +0530

    Integradted NFC module to Mega board

--- slider-plus/slider-plus.ino
+++ slider-plus/slider-plus.ino
@@ -7,7 +7,7 @@
 #include <RFID.h>
 #include <stdio.h>
 
-const int SS_PIN = 10;  //slave select pin
+const int SS_PIN = 53;  //slave select pin
 const int RST_PIN = 5;  //reset pin
 
 RFID rfid(SS_PIN, RST_PIN);
